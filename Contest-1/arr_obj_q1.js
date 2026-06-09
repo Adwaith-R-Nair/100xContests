1d618bed6a128ddc47b27364f5034a15e09f580d
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-06-09 15:25:43 +0530

    arr_obj_q1

---
+++ Contest-1/arr_obj_q1.js
@@ -0,0 +1,23 @@
+const transactions = [
+  { user: "A", amount: 100 },
+  { user: "B", amount: 200 },
+  { user: "A", amount: 50 }
+];
+
+// console.log(transactions[0].amount)
+// console.log(transactions[0].user)
+
+const user_transactions = []
+
+for(let i = 0; i < transactions.length; i++){
+    const {user, amount} = transactions[i];
+    if (user_transactions[user]){
+        user_transactions[user] += amount;
+    }
+    else{
+        user_transactions[user] = amount;
+    }
+}
+
+console.log(user_transactions);
+
